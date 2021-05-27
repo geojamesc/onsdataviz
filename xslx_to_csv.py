781b347a534092d45d0cf3632a2283368846748f
geojamesc <james.crone@ed.ac.uk>
2021-05-27 16:50:43 +0100

    tweeked

--- xslx_to_csv.py
+++ xslx_to_csv.py
@@ -17,11 +17,16 @@ def convert_data(xlsx_fn):
     sheet_names_w_pcnt_col_indexes = {
         'TradingStatus_TS': [4, 5, 6, 7, 8, 10, 11, 12],
         'FinancialPerformance_TS': [4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15],
-        'WorkforceStatus_TS': [3, 4, 5, 6, 7, 8, 10],
+        'WorkforceStatus_TS': [3, 4, 5, 6, 7, 8, 11],
         'CashFlow_TS': [4, 5, 6, 7, 8, 9, 11, 12]
     }
 
-
+    viz_priority_columns = {
+        'TradingStatus_TS': [8, 11, 12],
+        'FinancialPerformance_TS': [7, 14, 15],
+        'WorkforceStatus_TS': [3, 6, 7],  # TODO note has no number column for pd index used below
+        'CashFlow_TS': [12]
+    }
 
     if os.path.exists(xlsx_fn):
         for sheet in sheet_names_w_pcnt_col_indexes:
@@ -34,12 +39,19 @@ def convert_data(xlsx_fn):
                 # i.e. the_convertors[4] = format_cell
                 the_convertors[col] = format_cell
 
+            # use the first number column as the pandas index
+            pd_index_col = 0
+            if sheet == 'WorkforceStatus_TS':
+                # except in this case where there is no such column
+                pd_index_col = None
+
+
             # read the worksheet into a pandas dataframe
             df = pd.read_excel(
                 xlsx_fn,
                 sheet_name=sheet,
                 skiprows=9,  # skip the first 9 rows as these contain textual notes
-                index_col=0,  # use the first number column as the pandas index
+                index_col=pd_index_col,
                 na_values='*',  # NULL values
                 converters=the_convertors  # convert specified cols as per our defined above the_convertors dict
             )
